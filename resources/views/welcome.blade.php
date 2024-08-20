f1d29f67efbf5fb8b4d015c01e559fff11833632
umarmuhdhor <muhdhorcs@gmail.com>
2024-08-20 23:49:06 +0700

    Perubahan dikit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,34 +5,26 @@
     <title>Umar Muhdhor - Portfolio</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy"
+    content="upgrade-insecure-requests">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
     <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('css/animate.css')}}">
-    <link rel="stylesheet" href="css/animate.css">
 
     <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="css/magnific-popup.css">
 
     <link rel="stylesheet" href="{{asset('css/aos.css')}}">
-    <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
-    <link rel="stylesheet" href="css/ionicons.min.css">
 
     <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
     <link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/carousel.css')}}">
-    {{-- <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/carousel.css"> --}}
 
     <!-- Owl Carousel CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
@@ -106,7 +98,7 @@
                     <div class="row d-flex no-gutters slider-text align-items-end justify-content-end"
                         data-scrollax-parent="true">
                         <div class="one-third js-fullheight order-md-last img"
-                            style="background-image:url(images/bg_2.png);">
+                            style="background-image:url({{asset('images/bg_2.png')}});">
                             <div class="overlay"></div>
                         </div>
                         <div class="one-forth d-flex align-items-center ftco-animate"
@@ -884,18 +876,29 @@
         </svg></div>
 
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/aos.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="js/scrollax.min.js"></script>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/aos.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/jquery.animateNumber.min.js')}}"></script>
+    <script src=""></script>
+    <script src="{{asset('js/scrollax.min.js')}}"></script>
+    <script src=""></script>
 
     <script src="js/main.js"></script>
 
