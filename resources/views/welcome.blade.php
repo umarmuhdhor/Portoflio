ea46a42ff42fa8de1c472ca04f0275b87aefb404
umarmuhdhor <muhdhorcs@gmail.com>
2024-08-20 18:52:34 +0700

    tambahan dikit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,22 @@
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
+    <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
     <link rel="stylesheet" href="css/animate.css">
 
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
     <link rel="stylesheet" href="css/magnific-popup.css">
 
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
     <link rel="stylesheet" href="css/aos.css">
 
+    <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
     <link rel="stylesheet" href="css/ionicons.min.css">
 
     <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
@@ -74,7 +81,7 @@
                     <div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end"
                         data-scrollax-parent="true">
                         <div class="one-third js-fullheight order-md-last img"
-                            style="background-image:url(images/bg_22.png);">
+                            style="background-image:url({{asset('images/bg_22.png')}});">
                             <div class="overlay"></div>
                         </div>
                         <div class="one-forth d-flex  align-items-center ftco-animate"
